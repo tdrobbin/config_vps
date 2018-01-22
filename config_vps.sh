5cc398b772f443d9d19ced83cb7c8319a19d590b
Tyler Drobbin <tylerdrobbin@gmail.com>
2018-01-21 22:49:28 -0500

    Create config_vps.sh

---
+++ config_vps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set and persist sda_bas env var to use by other apps
+# export SDA_BASE = $HOME/sda_base
+echo export SDA_BASE = $HOME/sda_base >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# install miniconda
+curl -O https://repo.continuum.io/miniconda/Miniconda2-4.3.31-Linux-x86_64.sh
+source Miniconda2-4.3.31-Linux-x86_64.sh
+
+# activate installation
+source $HOME/.bashrc
+
+# install conda environments
+conda create -n py3 python=3
+
+# activate and run simpler server
+conda activate py3
+python -c "import sys; print(sys.executable)"
