8b64f293a3f696184ab0805f4a06fd97c8859ccb
Tyler Drobbin <tylerdrobbin@gmail.com>
2018-01-21 23:20:39 -0500

    Update config_vps.sh

--- config_vps.sh
+++ config_vps.sh
@@ -6,8 +6,12 @@ echo export SDA_BASE=$HOME/sda_base >> $HOME/.bashrc
 source $HOME/.bashrc
 
 # install miniconda
+# choose all default settings so everything after works
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+
+# add conda to path
+echo export PATH=$PATH:$HOME/miniconda3/bin >> $HOME/.bashrc
 
 # activate installation
 source $HOME/.bashrc
