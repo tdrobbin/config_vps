951bf829cd2c8ee38460b2c6b1379877057e30bb
Tyler Drobbin <tylerdrobbin@gmail.com>
2018-01-21 23:10:21 -0500

    Update config_vps.sh

--- config_vps.sh
+++ config_vps.sh
@@ -6,7 +6,7 @@ echo export SDA_BASE=$HOME/sda_base >> $HOME/.bashrc
 source $HOME/.bashrc
 
 # install miniconda
-curl -O https://repo.continuum.io/miniconda/Miniconda2-4.3.31-Linux-x86_64.sh
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda2-4.3.31-Linux-x86_64.sh
 
 # activate installation
